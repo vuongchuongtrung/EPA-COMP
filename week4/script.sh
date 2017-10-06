57ac53b8c27bf5a0592819e7327d5d5f2b3f3fd8
vuongchuongtrung <vuongchuongtrung@yahoo.ie>
2017-10-06 17:54:41 +0100

    add week4

---
+++ week4/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# lab4
+# two arguments passing
+count=2;
+if [ $# -lt $count ]; then
+	echo "too few arguments, exiting..."
+else
+	if [ -d $1 ]; then
+		echo "$1 directory existed"
+	else
+		mkdir $1
+		echo "$1 directory created"
+	fi
+
+	if [ -d $2 ]; then
+		echo "$2 directory existed"
+	else
+		#e.g.: /include/backupdir
+		mkdir $1$2
+		echo "$2 directory created"
+	fi
+fi
