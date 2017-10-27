905b85bd3dbb2ed2cdf4284db4fb28d270ed02f2
epa <epa@localhost.localdomain>
2017-10-27 09:40:02 +0100

    update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# This is just a simple script to commit/push
+# Git pull before add/commit/push
+printf "git pull"
+git pull
+printf "git status\n"
+git status
+printf "git add -A\n"
+git add -A
+printf "git commit\n"
+
 message=$1
 if [ -z $message ]; then
 	printf "Enter commit message: "
 	read message
 fi
-
-git add -A
 git commit -m "$message"
+printf "git push\n"
 git push
 
