f0ec354c3dcaf7899d092dff0f519b6129ec1e6b
vuongchuongtrung <vuongchuongtrung@yahoo.ie>
2017-10-28 18:30:56 +0100

    update instance_check.sh

--- week6/instance_check.sh
+++ week6/instance_check.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
 id1=${1}
-
 # if user do not pass in instance ids, default instance ids will be set
 if [ -n ${id} ]; then
 	id=i-033390d4b2e4fb049
 fi
-
-fileName=checkInstance.txt
-
 
 # status=`aws ec2 describe-instance-status --instance-ids ${id} --query InstanceStatuses[*].InstanceStatus`
 # append status to file
 # aws ec2 describe-instance-status --instance-ids ${id} >> ${fileName}
 # count "running", but now not used
 # run=`grep running ${fileName} | wc -l`
-
+fileName=checkInstance.txt
 aws ec2 describe-instance-status --instance-id ${id} > ${fileName}
 count=`grep running ${fileName} | wc -l`
 
 # get current date-time
 details=`date`
 # will override, just want to keep fresh details
-printf "Checked at: ${details}\n" > ${fileName}
+printf "Checked at: ${details}\n\n" > ${fileName}
 printf "Status: \n" >> ${fileName}
-
 
 if [ ${count} -gt 0 ]; then
 	printf "${id}: running\n" >> ${fileName}
