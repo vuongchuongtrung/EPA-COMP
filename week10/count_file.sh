85428ab0e4953f19dc22327d2c5b9992248b382d
epa <epa@localhost.localdomain>
2017-11-24 17:18:25 +0000

    update

--- week10/count_file.sh
+++ week10/count_file.sh
@@ -4,7 +4,5 @@ max=3
 cnt=`ls /tmp | wc -l`
 
 if [ ${cnt} -gt ${max} ]; then
-	echo "`date` Max number of files ${cnt} exceeded in /tmp" >> home/EPA-COMP/week10/log.txt
+	echo "`date`  |  Max number of files ${cnt}  |  exceeded in /tmp" >> /home/epa/EPA-COMP/week10/log.txt
 fi
-
-
--- week9/cpu_json.sh
+++ week9/cpu_json.sh
@@ -4,17 +4,5 @@
 # 2. the script uses pipe to jq to have jq identify the cpu-load value from mpstat
 # 3. the script uses jq to subtract this number from 100 to obtain the utilization of the cpu
 
-# -P 0: first cpu only (CPU 0)
-# Perform string is none-zero check (simple check)
-if [ -n $1 ]; then
-	if [ -n $2 ]; then
-		#utilisation=`mpstat -P 0 -o JSON $1 $2 | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-		utilisation=`mpstat -o JSON 10 1 | jq -C '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-		printf "utilisation = ${utilisation} \n"
-	else
-		printf "1\n"
-	fi
-else
-	printf "2\n"
-fi
-
+busyTime=`mpstat -o JSON 10 1 | jq -C '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+printf "Busy time = ${busyTime} \n"
